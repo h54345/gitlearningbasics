43ded43e602dfdae279e315b08652f949d5b24a9
hamzanaseem <hamza_nasim@live.com>
2019-06-12 16:51:29 +0530

    adding logger in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
+import java.util.logging.Logger;
 
+public class Main {
+    public static Logger logger= Logger.getLogger("message");
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
