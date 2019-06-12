6caadda0fa338cca89191ee588483974c02bab23
hamzanaseem <hamza_nasim@live.com>
2019-06-12 16:41:10 +0530

    adding logger

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,8 +1,12 @@
+import java.util.logging.Logger;
+
 public class NewClass {
 
-
+public static Logger logger= Logger.getLogger("Starting the New Class");
     public static void main(String args[]){
         System.out.println("Welcome to the world of gits");
 	System.out.println("test");
+
+
     }
 }
